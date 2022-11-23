b2c5b66811e4417bbfd8ec009455f6fd01003836
parovstelar <113955451+parovstelar@users.noreply.github.com>
2022-11-23 12:26:27 +0000

    [finish] 4-25

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,70 @@
+class BooksController < ApplicationController
+  # before_action :is_matching_login_user, only: [:edit, :update]
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def show
+    @books = Book.find(params[:id])
+    @users = @books.user
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  # def is_matching_login_user
+  #   user_id = params[:id].to_i
+  #   login_user_id = current_user.id
+  #   if(user_id != login_user_id)
+  #     redirect_to books_path
+  #   end
+  # end
+end
---
+++ app/models/book.rb
@@ -0,0 +1,6 @@
+class Book < ApplicationRecord
+
+  validates :title, presence: true
+  validates :body, length: { maximum: 200 }, presence: true
+  belongs_to :user
+end
